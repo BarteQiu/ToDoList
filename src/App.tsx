efb466595d10dc0ac675341dd0eea52059b53192
BarteQiu <bartekkwiatkowski2005@gmail.com>
2026-07-02 14:33:00 +0200

    feat: Add AddTask to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 
+
+interface Task {
+  id: number
+  text: string
+}
+
 function App() {
  
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [text, setText] = useState("")
+
+  const AddTask = () => {
+    if (!text.trim()) return
+
+    setTasks([...tasks, {id: Date.now(), text: text}])
+  }
+
 
 
   return (
-    <main className='bg-red-950 w-full h-screen'>
-      
+    <main className='bg-[#0c0c0c] w-full h-screen flex flex-col justify-center items-center text-white'>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+      className='border border-white'/>
+
+      <button onClick={AddTask}
+      className='border border-white cursor-pointer'>Add task</button>
 
     </main>
   )
